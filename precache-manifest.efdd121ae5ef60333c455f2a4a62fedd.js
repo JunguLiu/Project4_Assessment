599648d47730c29898315f7d3173943d174188bd
JunguLiu <33013871+JunguLiu@users.noreply.github.com>
2020-10-03 15:26:05 -0600

    Updates

---
+++ precache-manifest.efdd121ae5ef60333c455f2a4a62fedd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14f70a0e246f9f4fd4a821b506b92cf9",
+    "url": "/project-4-assessment/index.html"
+  },
+  {
+    "revision": "80fdaa51fa53457dad26",
+    "url": "/project-4-assessment/static/css/main.dbb2b88e.chunk.css"
+  },
+  {
+    "revision": "acc314c7a357911f8cd2",
+    "url": "/project-4-assessment/static/js/2.0d68efe0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/project-4-assessment/static/js/2.0d68efe0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80fdaa51fa53457dad26",
+    "url": "/project-4-assessment/static/js/main.c3eaa9b7.chunk.js"
+  },
+  {
+    "revision": "5f6f705e1e3b3b948cc9",
+    "url": "/project-4-assessment/static/js/runtime-main.3bb3fb77.js"
+  }
+]);
